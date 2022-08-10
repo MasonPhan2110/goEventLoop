034433ebafee303bf3cb3d91b652129ad4c61068
Phan Nhat Minh <minh.phan@ekoios.vn>
2022-08-10 15:00:52 +0700

    add thread

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	ev "github.com/minh211099/goEvenLoop/EventLoop"
+	"github.com/nguyenzung/go-event-loop/threadutils"
 )
 
 func main() {
+	fmt.Println("Main Thread ID: ", threadutils.ThreadID())
 	var app *ev.App = nil
 	fmt.Println(app)
 }
